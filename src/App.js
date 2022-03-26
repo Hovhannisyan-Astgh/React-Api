8ab396a6e23e3f1d8053cf50271419af34d64b4e
Hovhannisyan-Astgh <hovhannisyan.astgh98@gmail.com>
2022-03-26 15:12:15 +0400

    react api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [images, setImages] = useState([])
+  const [search, setSearch] = useState('')
+
+
+  const getImages = () => {
+
+    const url = `https://api.unsplash.com/search/photos?query=${search}&per_page=2&client_id=4fe4THTwZ92lS6TqEIkpLjIPTIZHZTYZyi7U5ZaL8ZM`;
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        setImages(data.results)
+      })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-content">
+
+      <div className='search'>
+
+        <input className='input' value={search} onChange={(e) => { setSearch(e.target.value) }}></input>
+        <button className='' src='button' onClick={getImages}>press</button>
+
+      </div>
+      <div className='allresults'>
+
+        {images.map((item) =>
+          <div className='result' key={item.id}>
+
+            <img src={item.urls.thumb} />
+            <p className='description'>{item.alt_description}</p>
+
+          </div>
+          
+        )}
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
